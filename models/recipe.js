ef86787874566fef7230dbc2c31d1c803e4a8de2
Eric Johnson <ericjohnson0216@icloud.com>
2020-11-06 21:46:23 -0500

    Done.

---
+++ models/recipe.js
@@ -0,0 +1,9 @@
+ //Models folder and create a new file called `recipe.js`.
+module.exports = function(sequelize, DataTypes) {
+  var Recipe = sequelize.define("Recipe", {
+    name: DataTypes.STRING,
+    ingredients: DataTypes.STRING,
+    content: DataTypes.TEXT,
+    });
+  return Recipe;
+};
